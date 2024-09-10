516c8280f5053fd37df011e676c09bad82553d00
methal <handsson@ewhain.net>
2024-09-10 10:45:48 +0900

    0910 second commit

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const loginInput = document.querySelector("#login-input");
-const loginButton = document.querySelector("#login-button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick() {
-  const username = loginInput.value;
-  console.dir(loginInput.value);
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault(); // 기본 동작인 폼 제출을 막음
+  loginForm.classList.add(HIDDEN_CLASSNAME); // 폼 숨김
+  const username = loginInput.value; // 입력된 이름 저장
+  console.log(username);
+  greeting.innerText = `Hello ${username}`; // 환영 메시지 표시
+  greeting.classList.remove(HIDDEN_CLASSNAME); // 환영 메시지 보이기
 }
-loginButton.addEventListener("click", onLoginBtnClick);
+
+loginForm.addEventListener("submit", onLoginSubmit); // 폼 제출 이벤트 리스너 추가
