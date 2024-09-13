8dd4e05e3fc51c44e497fd2dd98f51964c63c3ba
methal <handsson@ewhain.net>
2024-09-14 00:58:01 +0900

    saving todos in localstorage commit

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # chromeProject
 
-append()는 마지막에
-<form>: 사용 측에서 입력을 입력하는 HTML 양식을 정의
-기본적으로 html의 기본틀(form -> id/class) 정의 후 js에서 편집
-편집; createElement() -> innerText수정 -> eventListener("행동", 실행함수) -> append()
+li.remove(); 클릭 된 것은 동글이 리스트에 해당하는 li임
+-> console.dir(event.target)를 통해 알 수 있었음
+-> <li>가 아니라 .target만 삭제할 경우 <span>도 남아버림
+
+localStorage는 문자열만 저장 가능 받은 배열을 .stringify
+
+const toDos = [];
+// 요소 추가해도 value는 still array라 오류 없음
+// object & arr: reference type 
--- js/todo.js
+++ js/todo.js
@@ -2,8 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+// 요소 추가해도 value는 still array라 오류 없음
+// object & arr: reference type 
+
+function saveToDos() {
+    // localStorage에 toDos[]저장
+
+  localStorage.setItem("todos", JSON.stringify(toDos));
+    // localStorage는 문자열만 저장 가능 받은 배열을 .stringify
+}
+
 function deleteToDo(event) {
-    const li = event.target;
+    const li = event.target.parentElement;
     li.remove();
   }
 
@@ -25,7 +36,10 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+   // 엔터치면 해당 박스에서 글자 사라지도록
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
